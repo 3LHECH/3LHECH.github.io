d16360cfcd312a1462f6bbd7ba963f7595858d49
3LHECH <121363186+3LHECH@users.noreply.github.com>
2023-01-05 11:17:17 +0100

    Add files via upload

---
+++ 3LHECH.github.io/hechmi.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hid=document.querySelectorAll('.hidden');
+hid.forEach((el)=> observer.observe(el));
